21d80d2d341db77f7ce8809480c9cd0bd58421da
divya.ar <divya.ar@collabera.com>
2024-04-23 21:09:59 +0530

    Created Domain, DbContext and Migrated data

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,10 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+
+
+    }
+}
